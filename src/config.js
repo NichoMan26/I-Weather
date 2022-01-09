424d22e1bb5eb1bf09ea7299a164f0b49e2913db
Denis Basenko <44244294+NichoMan26@users.noreply.github.com>
2022-01-09 17:03:16 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const PORT = 'https://cors-anywhere.herokuapp.com/http://dataservice.accu
 
 
 //Basenkodenis@gmail.com
-// export const KEY = 'F4m7m0EAXku6BItSIsCv02ugHRfA7fI0'
+ export const KEY = 'F4m7m0EAXku6BItSIsCv02ugHRfA7fI0'
 
 //Basenkodenis.work@gmail.com
-export const KEY = 'jNc5zZA3lzS9Gj95Hz1GqVxRJ4Gn8zqI'
+//export const KEY = 'jNc5zZA3lzS9Gj95Hz1GqVxRJ4Gn8zqI'
